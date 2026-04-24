37ae3c9c0d9b0b8f2edd5aab5b3dd19c323f034f
Ismail Malik <vesmalick@gmail.com>
2026-04-24 16:00:59 +0300

    Completed varibles and data types challenge 04

---
+++ 01_variables_qstn_4.py
@@ -0,0 +1,15 @@
+# Challenge: Try casting: start with "25" as a string, cast it to int, multiply it, then cast the result back to a string.
+
+price_as_string = "25"
+price_as_int = int(price_as_string)
+quantity =  5
+total = price_as_int * quantity
+
+print()
+print("Casting Challenge: ")
+print()
+print(f"Price: {price_as_string}")
+print(f"Quantity: {quantity}")
+print(f"Total: {total}")
+print()
+
