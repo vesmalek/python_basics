2d0f5dd72a7d18b7127fe17e898b49b9896f45f8
Ismail Malik <vesmalick@gmail.com>
2026-04-24 11:29:00 +0300

    Completed variables challenge 01

---
+++ 01_variables_qstn_1.py
@@ -0,0 +1,12 @@
+# Challenge: Create variables for your name, age, city, and whether you're a developer. Print them all using an f-string.
+
+full_name = "Ismail Malik"
+age = 30
+city = "Dar es Salaam"
+is_developer = True
+
+print("\n")
+
+print(f"My name is {full_name}, I am {age} years old, I live in {city}, and I am {'a developer' if is_developer else 'not a developer'}.")
+
+print("\n")
