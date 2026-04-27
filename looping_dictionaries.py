81b4fdcb96d788168f0f09aa1ad000a08899a6af
Ismail Malik <vesmalick@gmail.com>
2026-04-27 19:18:02 +0300

    Practiced looping through dictionaries

---
+++ looping_dictionaries.py
@@ -0,0 +1,27 @@
+# Looping through Dictionaries
+
+car = {
+    "name": "IST",
+    "brand": "Toyota",
+    "year": "2006",
+    "country": "Japan"
+}
+
+# Looping through keys only
+print()
+print("Looping through keys only:")
+for key in car:
+    print(key)
+
+# looping through values only
+print()
+print("Looping through values only:")
+for value in car.values():
+    print(value)
+
+# looping through key and their values
+print()
+print("Looping through keys and values:")
+
+for key, value in car.items():
+    print(f"Key: {key}, Value: {value}")
