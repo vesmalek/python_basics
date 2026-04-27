295fa1074ab66d690595d3e36567abba205cfbd9
Ismail Malik <vesmalick@gmail.com>
2026-04-27 22:57:12 +0300

    Modified my data for python loops challenge 03

--- 05_loops_qstn_03.py
+++ 05_loops_qstn_03.py
@@ -1,6 +1,6 @@
 # Challenge 3: Given a list of usernames, loop through and stop the moment you find a username that starts with the letter "A" — print that username and a message saying it was found.
 
-usernames = ["Ali", "Hamad", "Issam", "Kassim", "Arshad", "Ahmed"]
+usernames = ["Gloria", "Hamad", "Issam", "Kassim", "Arshad", "Ahmed"]
 
 for username in usernames:
     if username.startswith("A"):
